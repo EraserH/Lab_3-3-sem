71362db5e5889d68a322efc97d7185e1a2d0926d
Oleg Chudilin <73231661+EraserH@users.noreply.github.com>
2020-12-10 08:16:37 +0400

    Update Vector.h
    косметический правки

--- Vector.h
+++ Vector.h
@@ -10,22 +10,17 @@ class Vector {
 	size_t _size;
 	size_t _capacity;
 
-	/*bool reallocate(size_t capacity) {
-		if (capacity < _capacity) {
-
-		}
-	}*/
 public:
-	Vector(): data(nullptr), _size(0), _capacity(0){
+	Vector() : data(nullptr), _size(0), _capacity(0) {
 
 	}
 
-	Vector(const Vector<T>& _vector): data(new T[_vector._size]), _size(_vector._size), _capacity(_vector._capacity) {
+	Vector(const Vector<T>& _vector) : data(new T[_vector._size]), _size(_vector._size), _capacity(_vector._capacity) {
 		for (auto i = 0; i < _size; ++i)
 			data[i] = _vector.data[i];
 	}
 
-	Vector(Vector<T>&& _vector): data(_vector.data), _size(_vector._size), _capacity(_vector._capacity) {
+	Vector(Vector<T>&& _vector) : data(_vector.data), _size(_vector._size), _capacity(_vector._capacity) {
 		_vector.data = nullptr;
 	}
 
@@ -35,7 +30,7 @@ public:
 		return *this;
 	}
 
-	Vector<T>& operator=(Vector<T>&& _vector) noexcept{
+	Vector<T>& operator=(Vector<T>&& _vector) noexcept {
 		swap(_vector);
 		return *this;
 	}
@@ -92,35 +87,11 @@ public:
 		std::swap(_capacity, _vector._capacity);
 	}
 
-	/*const T* insert(const T* pos, const T& value) {
-		if (pos < begin() || pos > end())
-			throw std::logic_error("Iterator out of range");
-
-		size_t posIndex = pos - data; //надо делить на размер или оно само автоматически? ПРОВЕРИТЬ
-		//так не работает (работает для pos = begin(), остальное - ПРОВЕРИТЬ)
-
-		if (_size + 1 > _capacity) {
-			if (_size == 0)
-				resize(1);
-			else
-				resize(size_t(ceil(double(_size) * 1.6))); // проверить
-		}
-		else
-			++_size;
-
-		for (size_t i = _size - 1; i > posIndex; --i) {
-			data[i] = std::move_if_noexcept(data[i - 1]); // проверить эту чепуху
-		}
-		data[posIndex] = value; //ИСКЛЮЧЕНИЕ: нарушение доступа для записи
-
-		return &data[posIndex];
-	}*/
-
 	const T* insert(const T* pos, const T& value) {
 		if (pos < begin() || pos > end())
 			throw std::logic_error("Iterator out of range");
 
-		size_t posIndex = pos - data; 
+		size_t posIndex = pos - data;
 
 		if (_size + 1 > _capacity) {
 			if (_size == 0)
@@ -135,7 +106,7 @@ public:
 		for (size_t i = _size - 1; i > posIndex; --i) {
 			indicator1 = data[i];
 			indicator2 = data[i - 1];
-			data[i] = std::move_if_noexcept(data[i - 1]); 
+			data[i] = std::move_if_noexcept(data[i - 1]);
 			indicator1 = data[i];
 			indicator2 = data[i - 1];
 		}
@@ -148,18 +119,18 @@ public:
 		if (pos < begin() || pos > end())
 			throw std::logic_error("Iterator out of range");
 
-		size_t posIndex = pos - data; 
+		size_t posIndex = pos - data;
 
 		for (size_t i = posIndex; i < _size - 1; ++i) {
-			data[i] = std::move_if_noexcept(data[i + 1]); 
+			data[i] = std::move_if_noexcept(data[i + 1]);
 		}
 		--_size;
 
 		return &data[posIndex];
 	}
 
-	void resize(size_t size, T value = T()) { 
-		if (size == _size) 
+	void resize(size_t size, T value = T()) {
+		if (size == _size)
 			return;
 
 		if (size <= _capacity) {
@@ -168,15 +139,12 @@ public:
 
 		else if (size > _capacity) {
 			auto tmp = new T[size];
-			/*if (_size != 0) //это костыль тащемта
-				for (size_t i = 0; i < size; ++i) //тут меньше _size наверное. ПРОВЕРИТЬ
-					tmp[i] = std::move_if_noexcept(data[i]); //тут вызывается исключение (нарушение доступа для чтения)*/
 			for (size_t i = 0; i < _size; ++i)
 				tmp[i] = std::move_if_noexcept(data[i]);
 			delete[] data;
 			data = tmp;
 			_size = size;
-			_capacity = size; 
+			_capacity = size;
 		}
 	}
 
